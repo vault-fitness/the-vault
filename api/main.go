1d6601e563b8135755f610273741f1fb08d2dff2
arambrosius <aambrosius@charter.net>
2024-04-22 15:47:36 -0500

    Added initial API with a database connection

--- api/main.go
+++ api/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/joho/godotenv"
+	"vaultfitness.io/api/common"
+)
 
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	common.Init()
 }
