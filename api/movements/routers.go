9bf4f5746fff49e029819595323a2a6726aec479
arambrosius <aambrosius@charter.net>
2024-04-26 07:44:01 -0500

    added an initial test query

--- api/movements/routers.go
+++ api/movements/routers.go
@@ -4,12 +4,25 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"vaultfitness.io/api/common"
 )
 
 func MovementsRegister(router *gin.RouterGroup) {
+
 	router.GET("/", MovementsList)
 }
 
+type Result struct {
+	ID           int
+	Name         string
+	Abbreviation string
+}
+
 func MovementsList(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"movements": nil})
+	db := common.GetDB()
+
+	var result []Result
+	db.Raw("SELECT id, name, abbreviation FROM movement_types").Scan(&result)
+
+	c.JSON(http.StatusOK, result)
 }
