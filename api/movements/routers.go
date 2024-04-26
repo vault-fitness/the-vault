8602c73ed34fe7d614f6b478ba790206a047aa76
Adam Ambrosius <adam@Adams-Mac-mini.local>
2024-04-26 08:06:47 -0500

    added movement models

--- api/movements/routers.go
+++ api/movements/routers.go
@@ -4,25 +4,34 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"vaultfitness.io/api/common"
 )
 
 func MovementsRegister(router *gin.RouterGroup) {
 
-	router.GET("/", MovementsList)
+	router.GET("/type", MovementTypesList)
 }
 
-type Result struct {
-	ID           int
-	Name         string
-	Abbreviation string
+type MovementType struct {
+	gorm.Model
+	Name         string `gorm:"unique; not null"`
+	Abbreviation string `gorm:"unique; not null"`
 }
 
-func MovementsList(c *gin.Context) {
+type Movement struct {
+	gorm.Model
+	Name           string `gorm:"unique; not null"`
+	Abbreviation   string `gorm:"unique; not null"`
+	ImageUrl       string
+	MovementTypeID uint
+}
+
+func MovementTypesList(c *gin.Context) {
 	db := common.GetDB()
 
-	var result []Result
-	db.Raw("SELECT id, name, abbreviation FROM movement_types").Scan(&result)
+	var mt []MovementType
+	db.Raw("SELECT id, name, abbreviation FROM movement_types").Scan(&mt)
 
-	c.JSON(http.StatusOK, result)
+	c.JSON(http.StatusOK, mt)
 }
